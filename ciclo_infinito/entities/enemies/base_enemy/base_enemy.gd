59aeb9e400ab8f43e3b23784997f5472e3d2a650
Rebeca <dpfrebeca@gmail.com>
2026-04-07 20:31:25 -0300

    Correção de última hora no bug3

--- ciclo_infinito/entities/enemies/base_enemy/base_enemy.gd
+++ ciclo_infinito/entities/enemies/base_enemy/base_enemy.gd
@@ -4,6 +4,10 @@ extends CharacterBody2D
 
 signal defeated ##Contador para a tela de vitória
 
+#Para não grudar
+@export_category("Attributes")
+@export var is_floating: bool = true
+
 
 @export_category("objects")
 @export var sprite: Sprite2D = null
