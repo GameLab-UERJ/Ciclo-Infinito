96aea988fc03be251b89870d3b6552dd38b6c78f
JPLMB <145187137+JPLMB@users.noreply.github.com>
2026-04-12 03:12:06 -0300

    style(tipagem): Mudanças de guia de estilo (sem lógica alterada)

--- ciclo_infinito/systems/music_manager.gd
+++ ciclo_infinito/systems/music_manager.gd
@@ -1,14 +1,14 @@
 extends Node
-var music_player := AudioStreamPlayer.new()
+var music_player: AudioStreamPlayer = AudioStreamPlayer.new()
 
 func _ready() -> void:
 	add_child(music_player)
 
-func play_music(stream: AudioStream):
+func play_music(stream: AudioStream) -> void:
 	if music_player.stream == stream and music_player.playing:
 		return
 	music_player.stream = stream
 	music_player.play()
 
-func stop_music():
+func stop_music() -> void:
 	music_player.stop()
