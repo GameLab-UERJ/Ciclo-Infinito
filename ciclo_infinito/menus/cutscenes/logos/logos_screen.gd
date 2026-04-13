6dd9caed5072ef156c055ceb4921365d325168c1
Clique33 <gabriegcc@gmail.com>
2026-04-13 14:44:26 -0300

    feat(logo_screen): added single stream for music in logo screen up to the main menu.

--- ciclo_infinito/menus/cutscenes/logos/logos_screen.gd
+++ ciclo_infinito/menus/cutscenes/logos/logos_screen.gd
@@ -2,6 +2,10 @@ extends VideoStreamPlayer
 
 var main_menu : PackedScene = preload("uid://downt2rxxaqaf")
 
+func _ready() -> void:
+	var music : AudioStream = preload("uid://c8ag6husw1v2")
+	MusicManager.play_music(music,8)
+
 func _on_finished() -> void:
 	get_tree().change_scene_to_packed(main_menu)
 	pass # Replace with function body.
