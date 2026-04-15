c13a44dbc7a5e896990d8eadd3ce3bd18392ceef
Rebeca <dpfrebeca@gmail.com>
2026-04-14 21:43:19 -0300

    feat(menu de controle):Acredito que fiz os reajustes. Seria bom verificar

--- ciclo_infinito/menus/controls/menu_controles.gd
+++ ciclo_infinito/menus/controls/menu_controles.gd
@@ -1,17 +1,5 @@
 extends Control
 
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_voltar_pressed() -> void:
 	if get_parent() != get_tree().root:
 		var pause_menu = get_parent().get_node_or_null("MarginContainer")
