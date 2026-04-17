d9aa6fa31d4de55f944bd91f75df2820be37e237
Clique33 <gabriegcc@gmail.com>
2026-04-17 18:33:20 -0300

    feat(fire_meteor): completed

--- ciclo_infinito/entities/enemies/bosses/herald_of_failure/attacks/fire_meteor.gd
+++ ciclo_infinito/entities/enemies/bosses/herald_of_failure/attacks/fire_meteor.gd
@@ -2,36 +2,69 @@ class_name FireMeteor
 extends Node2D
 
 
-signal finished_drop
+signal deal_damage_to_player
 
 
 @export var initial_y_position : float = -100
 @export var total_drop_time : float = 1.0
 
 
-var final_y_position : float = -14
+var _final_y_position : float = -10
+var _final_impact_scale : Vector2
+var _player_damageable : bool = false
 
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var fire_ball_sprite: AnimatedSprite2D = $FireBallSprite
 @onready var shadow_sprite: Sprite2D = $ShadowSprite
+@onready var impact_sprite: Sprite2D = $ImpactSprite
 
 
 func _ready() -> void:
+	_final_impact_scale = impact_sprite.scale
 	reset()
 
-
-func _process(delta: float) -> void:
+#Only for testing. To be removed
+func _process(_delta: float) -> void:
 	if Input.is_action_just_released("ui_accept"):
 		reset()
 		drop()
 
 func reset() -> void:
+	impact_sprite.scale = Vector2.ZERO
 	shadow_sprite.scale = Vector2.ZERO
+	fire_ball_sprite.play("drop")
 	fire_ball_sprite.position.y = initial_y_position
 
 func drop() -> void:
 	var drop_tween : Tween = create_tween()
-	drop_tween.tween_property(fire_ball_sprite,"position",Vector2(fire_ball_sprite.position.x,final_y_position),total_drop_time)
+	drop_tween.tween_property(fire_ball_sprite,"position",Vector2(fire_ball_sprite.position.x,_final_y_position),total_drop_time)
 	drop_tween.parallel().tween_property(shadow_sprite,"scale",Vector2.ONE,total_drop_time)
-	drop_tween.finished.connect(finished_drop.emit)
+	drop_tween.finished.connect(_on_drop_finished)
+
+
+func _on_drop_finished() -> void:
+	fire_ball_sprite.play("disappear")
+	var temp_impact_self_modulate : Color = impact_sprite.self_modulate
+	temp_impact_self_modulate.a = 1
+	
+	create_tween().tween_property(
+		impact_sprite,
+		"scale",
+		_final_impact_scale,
+		0.2
+	)
+	if _player_damageable:
+		deal_damage_to_player.emit()
+
+func _on_damage_area_body_entered(body: Node2D) -> void:
+	if body is Player:
+		_player_damageable = true
+
+
+func _on_damage_area_body_exited(body: Node2D) -> void:
+	if body is Player:
+		_player_damageable = false
+
+
+func _on_fire_ball_sprite_animation_finished() -> void:
+	pass
