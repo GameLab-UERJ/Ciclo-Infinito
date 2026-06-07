ccb9e407eed2e7fcad2811826be997e5a811484d
Prof. Gabriel <uerjgamelab@gmail.com>
2026-06-07 00:10:08 -0300

    feat(lift): standardized lift's interact hint

--- ciclo_infinito/entities/interactables/lift/lift.gd
+++ ciclo_infinito/entities/interactables/lift/lift.gd
@@ -5,7 +5,7 @@ extends Area2D
 var player_in_area = false
 
 
-@onready var label_interação: Label = $LabelInteração
+@onready var label_interação: Label = $LabelInteracao
 @onready var cheese_hall : Cheesehall = $".."
 @onready var sprite: Sprite2D = $Sprite
 
@@ -25,7 +25,6 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player") and cheese_hall.jose.player_has_talked_with: 
 		player_in_area = true
 		sprite.material.set("shader_parameter/width",2)
-		label_interação.text = "Pressione 'E' para usar"
 		label_interação.visible = true
 
 func _on_body_exited(body: Node2D) -> void:
