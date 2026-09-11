0667eab2ebf3aa917a752c0680e71ee34cb4290c
gabriel <gabrielc@eng.uerj.br>
2026-09-11 11:40:15 -0300

    resource(marie_curie): added marie curie as resource and associated boons to sages automatically.

--- ciclo_infinito/resources/data/boons/boon.gd
+++ ciclo_infinito/resources/data/boons/boon.gd
@@ -7,6 +7,7 @@ enum AffectedSkill {PASSIVE, ATTACK1, ATTACK2, DASH, DASH_ATTACK, RANGED, ULTIMA
 
 @export var name : String
 @export var description : String
+@export var sage : Sage
 @export var affected_skill : Array[AffectedSkill]
 @export var common_modifier : AttributeModifier
 @export var uncommon_modifier : AttributeModifier
--- ciclo_infinito/resources/data/sages/sage.gd
+++ ciclo_infinito/resources/data/sages/sage.gd
@@ -5,3 +5,25 @@ class_name Sage
 @export var name : String
 @export var description : String
 @export var splash : Texture2D
+@export var boons_path : String
+
+
+var boons : Array[Boon] = [] : get = _get_boons
+
+
+func _set_boons_path(value : String) -> void:
+	boons_path = value
+	_get_boons() 
+	print(boons)
+
+
+func _get_boons() -> Array[Boon]:
+	if not boons.is_empty():
+		return boons
+	
+	var boon_files : PackedStringArray = DirAccess.get_files_at(boons_path)
+	for file_name in boon_files:
+		print(file_name)
+		print(boons_path + file_name)
+		boons.append(ResourceLoader.load(boons_path + file_name))
+	return boons
